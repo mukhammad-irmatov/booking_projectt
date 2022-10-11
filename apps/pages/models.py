75949616210b725c7bb132126aa535b81427578b
BozorboyevSamandar <bscoders@gmail.com>
2022-10-11 11:54:46 +0300

    add contacts form

--- apps/pages/models.py
+++ apps/pages/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from phone_field import PhoneField
+
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(blank=True, null=True)
+    phone = PhoneField(max_length=9, default='94', blank=False, unique=True, help_text='Contact phone number')
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+
+from .models import Contact
+from .forms import ContactForm
 
 
 # Create your views here.
@@ -6,24 +10,46 @@ from django.shortcuts import render
 
 def home(request):
     context = {}
-    return render(request, 'index.html', context)
+    return render(request, 'pages/index.html', context)
 
 
 def rooms(request):
     context = {}
-    return render(request, 'rooms.html', context)
+    return render(request, 'pages/rooms.html', context)
 
 
 def about(request):
     context = {}
-    return render(request, 'about-us.html', context)
+    return render(request, 'pages/about-us.html', context)
 
 
 def blog(request):
     context = {}
-    return render(request, 'blog.html', context)
+    return render(request, 'pages/blog.html', context)
 
 
-def contact(request):
-    context = {}
-    return render(request, 'contact.html', context)
+class Contact(View):
+    def get(self, request):
+        contact_form = ContactForm()
+        context = {'form': contact_form}
+        return render(request, 'pages/contact.html', context)
+
+    def post(self, request):
+        contact_form = ContactForm(data=request.POST)
+        if contact_form.is_valid():
+            name = request.POST['name']
+            email = request.POST['email']
+            phone = request.POST['phone']
+            message = request.POST['message']
+
+            user_message = Contact.objects.create(
+                name=name,
+                email=email,
+                phone=phone,
+                message=message,
+            )
+            user_message.save()
+            return redirect('contact')
+        else:
+            context = {'form': contact_form}
+            return render(request, 'pages/contact.html', context)
